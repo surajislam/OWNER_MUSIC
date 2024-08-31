53c5f59812047dc8824bee35db041a533c372402
surajislam <154444093+surajislam@users.noreply.github.com>
2024-08-31 22:28:03 +0530

    Update zowner.py

--- BrandrdXMusic/cplugin/zowner.py
+++ BrandrdXMusic/cplugin/zowner.py
@@ -41,7 +41,7 @@ async def repo(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "🌱 𝐒𝐎𝐔𝐑𝐂𝐄 🌱", url=f"https://t.me/learningbots79"
+                        "🌱 𝐒𝐎𝐔𝐑𝐂𝐄 🌱", url=f"https://t.me/II_SB_SIMPLE_II"
                     )
                 ]
             ]
@@ -59,7 +59,7 @@ async def help(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "🌱 𝐒𝐎𝐔𝐑𝐂𝐄 🌱", url=f"https://t.me/learningbots79"
+                        "🌱 𝐒𝐎𝐔𝐑𝐂𝐄 🌱", url=f"https://t.me/II_SB_SIMPLE_II"
                     )
                 ]
             ]
