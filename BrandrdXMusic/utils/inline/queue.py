bfb4d494fe1c8e46af23cc6d9bec6a1cdd80b309
surajislam <154444093+surajislam@users.noreply.github.com>
2024-09-05 13:33:21 +0530

    Update queue.py

--- BrandrdXMusic/utils/inline/queue.py
+++ BrandrdXMusic/utils/inline/queue.py
@@ -73,10 +73,10 @@ def aq_markup(_, chat_id):
         ],
         [
             InlineKeyboardButton(
-                text="⛦ ᴏᴡɴᴇʀ ⛦", url=f"https://t.me/innocent_babe_dead"
+                text="⛦ ᴏᴡɴᴇʀ ⛦", url=f"https://t.me/Shadow_oopss"
             ),
             InlineKeyboardButton(
-                text="🥀 ꜱᴜᴘᴘᴏʀᴛ 🥀", url=f"https://t.me/learning_bots"
+                text="🥀 ꜱᴜᴘᴘᴏʀᴛ 🥀", url=f"https://t.me/II_ANIKA_PFP_II"
             ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
